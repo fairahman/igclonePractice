3c021a10492d1ac53c29ac6fcf0480344d619a72
frahman <faisalrahman3141@gmail.com>
2023-03-23 23:46:40 -0400

    sending over to the pc

---
+++ login.js
@@ -0,0 +1,20 @@
+const loginBtn = document.querySelector('#login');
+const accs = [{
+    username: 'Nick',
+    password: 'abc123',
+    profilePic: 'insert pic link',
+    imageLink: 'insert link',
+    likes: 52,
+    imageDescription: 'insert description'
+}, {
+    username: 'Rick',
+    password: 'abc123',
+    profilePic: 'insert pic link',
+    imageLink: 'insert link',
+    likes: 52,
+    imageDescription: 'insert description'} ]
+
+loginBtn.addEventListener('click', (e) => {
+    const userName =  document.querySelector('#username').value;
+    if ()
+})
